9dcb815c3e928ccb0a1335907cdd816a9ee9abaa
Oscar Hernandez <itzmeoskar@gmail.com>
2025-06-27 23:43:36 -0400

    First Commit

---
+++ main.js
@@ -0,0 +1,17 @@
+// Swiper for images
+var swiper = new Swiper(".home", {
+    loop: true,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+
+    
+const menuIcon = document.getElementById('menu-icon');
+const navbar = document.querySelector('.navbar');
+
+menuIcon.addEventListener('click', () => {
+  navbar.classList.toggle('active');
+});
+
